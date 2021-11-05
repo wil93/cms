e210cc20647f36c56056897df869c2d0e2259ccb
Edoardo Morassutto <edoardo.morassutto@gmail.com>
2021-11-05 20:59:42 +0100

    Update Dockerfiles

--- docker/worker/root/install-compilers.sh
+++ docker/worker/root/install-compilers.sh
@@ -2,28 +2,10 @@
 
 set -ex
 
-# C/C++
-apk add --no-cache \
-    gcc \
-    g++
+apt update
+apt install -yy \
+    fp-compiler \
+    pypy3
 
-# Pascal
-export FPC_VERSION="3.2.0"
-export FPC_ARCH="x86_64-linux"
-apk add --no-cache binutils
-cd /tmp
-wget "https://downloads.sourceforge.net/project/freepascal/Linux/${FPC_VERSION}/fpc-${FPC_VERSION}-${FPC_ARCH}.tar" -O fpc.tar
-tar xf "fpc.tar"
-cd "fpc-${FPC_VERSION}-${FPC_ARCH}"
-rm demo* doc*
-# Workaround musl vs glibc entrypoint for `fpcmkcfg`
-mkdir /lib64
-ln -s /lib/ld-musl-x86_64.so.1 /lib64/ld-linux-x86-64.so.2
-echo -e '/usr\nN\nN\nN\n' | sh ./install.sh
-find "/usr/lib/fpc/${FPC_VERSION}/units/${FPC_ARCH}/" -type d -mindepth 1 -maxdepth 1 \
-    -not -name 'fcl-base' \
-    -not -name 'rtl' \
-    -not -name 'rtl-console' \
-    -not -name 'rtl-objpas' \
-    -exec rm -r {} \;
-rm -r "/lib64" "/tmp/"*
+# Cleanup
+rm -rf /var/lib/apt/lists/*
